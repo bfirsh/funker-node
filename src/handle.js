cda6ddf27b3d7af59fd6872985a73f8cd95bce95
Alex Ellis <alexellis2@gmail.com>
2016-12-09 23:02:07 +0000

    Strings suck

--- src/handle.js
+++ src/handle.js
@@ -4,14 +4,18 @@ const net = require('net');
 
 let handler = (func, callback) => {
   let server = net.createServer({allowHalfOpen: true}, (connection) => {
-    let buf = '';
+    var buffer = new Buffer(0);
 
     connection.on('data', (data) => {
-      buf += data;
+      var tempBuffer = new Buffer(data.length);
+      tempBuffer.write(data.toString(), "utf8");
+      buffer = Buffer.concat([buffer, tempBuffer]);
     });
 
     connection.on('end', () => {
+      let buf = buffer.toString("utf8");
       let args = JSON.parse(buf);
+
       func(args, (resp) => {
         // If there is no return value, set it to something JSON serializable
         if (resp === undefined) {
@@ -20,12 +24,14 @@ let handler = (func, callback) => {
 
         connection.end(JSON.stringify(resp));
         server.close();
+        buffer = new Buffer(0);
       });
     });
   });
 
   server.on('error', (err) => {
     callback(err);
+    buffer = new Buffer(0);
   });
   
   server.listen(process.env.funker_port || 9999, '0.0.0.0', 1, callback);
